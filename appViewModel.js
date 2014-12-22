1628d8ada94e4002e07f89e7a217de9284422b7b
Rafał Kostrzewski <poczta.raf.kos@gmail.com>
2014-12-22 23:42:12 +0100

    Updated for The Next Day + CSS Bindings

--- appViewModel.js
+++ appViewModel.js
@@ -17,9 +17,9 @@ function getData()
 {
 	//defaults
 	var data = {
-		leftHandWords: "I",
+		leftWords: "I",
 		insideCircleWords: "Love",
-		rightHandWords: "You",
+		rightWords: "You",
 		bottomSentence:  "My sweet Magdalena",
 		
 		songTitle: "Shine On You Crazy Diamond",
@@ -32,7 +32,20 @@ function getData()
 		artUrl: "http://upload.wikimedia.org/wikipedia/commons/9/94/Starry_Night_Over_the_Rhone.jpg",
 		artistName: "Van Gogh",
 		artistUrl: "http://en.wikipedia.org/wiki/Vincent_van_Gogh",
-		artNote: "It doesn't get better than this!"
+		artNote: "It doesn't get better than this!",
+		
+		upperSentenceStyle: "sentence first",
+		leftWordsStyle: "outsideCircleText",
+		centerAdornment: "circle",
+		centerWordsStyle: "insideCircleText",
+		rightWordsStyle: "outsideCircleText",
+		
+		bottomSentenceStyle: "sentence",
+		bottomTextStyle: "bottomText",
+		
+		imageContainerStyle: "none",
+		imageStyle: "none",
+		imageSource: ""
 	};
 
 	try{
@@ -56,9 +69,9 @@ function getData()
 
 		var currentId = dd + "/" + mm + "/" + yyyy;
 
-		data.leftHandWords = xmlDoc.getElementById(currentId).getElementsByTagName("Text")[0].getElementsByTagName("Left")[0].childNodes[0].nodeValue;
+		data.leftWords = xmlDoc.getElementById(currentId).getElementsByTagName("Text")[0].getElementsByTagName("Left")[0].childNodes[0].nodeValue;
 		data.insideCircleWords = xmlDoc.getElementById(currentId).getElementsByTagName("Text")[0].getElementsByTagName("Center")[0].childNodes[0].nodeValue;
-		data.rightHandWords = xmlDoc.getElementById(currentId).getElementsByTagName("Text")[0].getElementsByTagName("Right")[0].childNodes[0].nodeValue;
+		data.rightWords = xmlDoc.getElementById(currentId).getElementsByTagName("Text")[0].getElementsByTagName("Right")[0].childNodes[0].nodeValue;
 		data.bottomSentence = xmlDoc.getElementById(currentId).getElementsByTagName("Text")[0].getElementsByTagName("Bottom")[0].childNodes[0].nodeValue;
 
 		data.songTitle = xmlDoc.getElementById(currentId).getElementsByTagName("Song")[0].getElementsByTagName("Title")[0].childNodes[0].nodeValue;
@@ -72,6 +85,24 @@ function getData()
 		data.artistName = xmlDoc.getElementById(currentId).getElementsByTagName("Art")[0].getElementsByTagName("Artist")[0].childNodes[0].nodeValue;
 		data.artistUrl = xmlDoc.getElementById(currentId).getElementsByTagName("Art")[0].getElementsByTagName("ArtistUrl")[0].childNodes[0].nodeValue;
 		data.artNote = xmlDoc.getElementById(currentId).getElementsByTagName("Art")[0].getElementsByTagName("Note")[0].childNodes[0].nodeValue;
+		try
+		{
+			data.upperSentenceStyle = xmlDoc.getElementById(currentId).getElementsByTagName("CSS")[0].getElementsByTagName("UpperSentenceStyle")[0].childNodes[0].nodeValue;
+			data.leftWordsStyle = xmlDoc.getElementById(currentId).getElementsByTagName("CSS")[0].getElementsByTagName("LeftWordsStyle")[0].childNodes[0].nodeValue;
+			data.centerAdornment = xmlDoc.getElementById(currentId).getElementsByTagName("CSS")[0].getElementsByTagName("CenterAdornmentStyle")[0].childNodes[0].nodeValue;
+			data.centerWordsStyle = xmlDoc.getElementById(currentId).getElementsByTagName("CSS")[0].getElementsByTagName("CenterWordsStyle")[0].childNodes[0].nodeValue;
+			data.rightWordsStyle = xmlDoc.getElementById(currentId).getElementsByTagName("CSS")[0].getElementsByTagName("RightWordsStyle")[0].childNodes[0].nodeValue;
+			
+			data.bottomSentenceStyle = xmlDoc.getElementById(currentId).getElementsByTagName("CSS")[0].getElementsByTagName("BottomSentenceStyle")[0].childNodes[0].nodeValue;
+			data.bottomTextStyle = xmlDoc.getElementById(currentId).getElementsByTagName("CSS")[0].getElementsByTagName("BottomTextStyle")[0].childNodes[0].nodeValue;
+			
+			data.imageSource = xmlDoc.getElementById(currentId).getElementsByTagName("Image")[0].getElementsByTagName("ImageSource")[0].childNodes[0].nodeValue;
+			data.imageContainerStyle = xmlDoc.getElementById(currentId).getElementsByTagName("Image")[0].getElementsByTagName("ImageContainerStyle")[0].childNodes[0].nodeValue;
+			data.imageStyle = xmlDoc.getElementById(currentId).getElementsByTagName("Image")[0].getElementsByTagName("ImageStyle")[0].childNodes[0].nodeValue;
+		}
+		catch (err)
+		{
+		}
 	}
 	catch(err)
 	{
@@ -81,13 +112,13 @@ function getData()
 	return data;
 }
 
-function AppViewModel()
+function AppDataModel()
 {
 	this.data = getData();
 	
-	this.leftHandWords = this.data.leftHandWords;
+	this.leftWords = this.data.leftWords;
 	this.insideCircleWords = this.data.insideCircleWords;
-	this.rightHandWords = this.data.rightHandWords;
+	this.rightWords = this.data.rightWords;
 	this.bottomSentence = this.data.bottomSentence;
 	
 	this.songTitle = this.data.songTitle;
@@ -101,6 +132,19 @@ function AppViewModel()
 	this.artistName = this.data.artistName;
 	this.artistUrl = this.data.artistUrl;
 	this.artNote = this.data.artNote;
+	
+	this.upperSentenceStyle = this.data.upperSentenceStyle;
+	this.leftWordsStyle = this.data.leftWordsStyle;
+	this.centerAdornment = this.data.centerAdornment;
+	this.centerWordsStyle = this.data.centerWordsStyle;
+	this.rightWordsStyle = this.data.rightWordsStyle;
+	
+	this.bottomSentenceStyle = this.data.bottomSentenceStyle;
+	this.bottomTextStyle = this.data.bottomTextStyle;
+	
+	this.imageContainerStyle = this.data.imageContainerStyle;
+	this.imageStyle = this.data.imageStyle;
+	this.imageSource = this.data.imageSource;
 }
 
-ko.applyBindings(new AppViewModel());
+ko.applyBindings(new AppDataModel());
